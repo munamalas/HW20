e7194221e4f1a918690ed4f61f14593a67b620b2
munamalas <34921110+munamalas@users.noreply.github.com>
2020-01-19 11:06:01 -0800

    HW 20
    Code a Case Study.  Works on Desktop.  Has some issues with mobile (homepage is responsive).  Currently working on portfolio page.  Started a basic layout for contact me page.

---
+++ index.js
@@ -0,0 +1,36 @@
+//Preloader animation
+function fade(){
+    $(".preloader").fadeOut("slow")
+};
+setTimeout(fade, 4000);
+
+//Collapsable Menu
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  //Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
